a50a1f6ba888e6c27d66ac6a9190c8390dd56aba
Narval1 <matheushsm51@gmail.com>
2023-09-06 15:27:13 -0300

    chore: organized imports

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
 import { AxiosHttpClient } from './axios-http-client'
+import { type HttpPostClientParams } from '@/data/protocols/http'
 import axios from 'axios'
 import { faker } from '@faker-js/faker'
-import { type HttpPostClientParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
