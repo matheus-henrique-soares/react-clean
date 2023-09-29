234027deff5e1bb1ec7ea9182d7eb235da71c87b
Narval1 <matheushsm51@gmail.com>
2023-09-29 11:55:49 -0300

    test: ensure loagin pesent spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -79,4 +79,15 @@ describe('Login component.', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
   })
+  test('Should show spinner on submit.', () => {
+    const { sut } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const passwordIpunt = sut.getByTestId('password')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    fireEvent.input(passwordIpunt, { target: { value: faker.internet.password() } })
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = sut.getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
