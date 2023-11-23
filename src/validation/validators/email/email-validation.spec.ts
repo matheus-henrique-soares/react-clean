dcd9825839aed8058b0e705168cc576f989195ba
Narval1 <matheushsm51@gmail.com>
2023-11-23 14:07:48 -0300

    fix: ensure EmailValidation allopws empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -29,4 +29,9 @@ describe('Email Validation', () => {
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
+  test('should return falsy if email is empty.', () => {
+    const { sut } = makeSut()
+    const error = sut.validate('')
+    expect(error).toBeFalsy()
+  })
 })
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -7,7 +7,7 @@ export class EmailValidation implements FieldValidation {
   validate (email: string): Error {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     const error = emailRegex.test(email.toLowerCase())
-    if (!error) return new InvalidFieldError(this.field)
-    return null
+    if (error || !email) return null
+    return new InvalidFieldError(this.field)
   }
 }
