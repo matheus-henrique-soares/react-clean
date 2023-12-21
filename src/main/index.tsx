f5c884399270c141d018c2a37a47b1444746f192
Narval1 <matheushsm51@gmail.com>
2023-12-21 11:28:29 -0300

    feat: a lot

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from '@/presentation/components'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import '@/presentation/styles/global.scss'
 
 const root = createRoot(document.getElementById('main'))
 
-root.render(<Router/>)
+root.render(<Router makeLogin={makeLogin}/>)
